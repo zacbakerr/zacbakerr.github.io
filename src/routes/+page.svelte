4d41a917cb1b81cd2339ce6d2be63fceccd21bf6
Zac Baker <82305525+zacbakerr@users.noreply.github.com>
2025-06-20 01:55:31 -0700

    updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,40 +5,52 @@
 
   const featuredProjects = [
     {
+      name: "Terac",
+      href: "https://terac.ai/",
+      desc: "Automating the exchange of human thoughts through agent-driven market research",
+      aside: "June 2025 - Present",
+    },
+    {
+      name: "ZFellows",
+      href: "https://zfellows.com/",
+      desc: "A fast-track to Silicon Valley brining founders together for a one-week experience.",
+      aside: "June 2025",
+    },
+    {
       name: "Radical Health",
       href: "https://radicalhealth.ai/",
       desc: "Founding engineer building foundation models for cancer",
-      aside: "2025-Present, backed $5M+ Khosla, partnered with Mayo Clinic",
+      aside: "Feb 2025 - June 2025, backed $5M+ Khosla, partnered with Mayo Clinic",
     },
     {
       name: "MIT CSAIL",
       href: "https://www.csail.mit.edu/",
       desc: "Trained foundation audio and video models",
-      aside: "2024-Present",
+      aside: "June 2024 - June 2025",
     },
     {
       name: "UCL RPL",
       href: "https://rpl-as-ucl.github.io/",
       desc: "Taught robots to think",
-      aside: "2024-2025",
+      aside: "2024 - 2025",
     },
     {
       name: "University College London",
       href: "https://www.ucl.ac.uk/",
       desc: "University",
-      aside: "2024-Dropped out",
+      aside: "2024 - Dropped out",
     },
     {
       name: "Zeed",
       href: "https://zeed.ai/",
       desc: "Founding engineer, scaled from $0-$10M+",
-      aside: "2021-2023",
+      aside: "2021 - 2023",
     },
     {
       name: "TJHSST",
       href: "https://tjhsst.edu",
       desc: "High school",
-      aside: "2020-2024",
+      aside: "2020 - 2024",
     },
   ];
 </script>
@@ -60,9 +72,9 @@
 <div class="layout-md text-lg space-y-14">
   <div class="space-y-5">
     <p>
-      I currently work at <a class="link" href="https://radicalhealth.ai/"
-        >Radical</a
-      >, training foundational models for cancer.
+      I'm currently building <a class="link" href="https://terac.ai/"
+        >Terac</a
+      >, automating the exchange of human thoughts through agent-driven market research.
     </p>
 
     <p>
@@ -73,7 +85,7 @@
     <p>
       Towards that goal, I am a generalist and care deeply about building <em
         >systems</em
-      >, which will move society towards a more fulfilling future.
+      >, which will move society towards a more fulfilling future. To start, I'm automating market research with the hopes of giving agents a pulse on societies wants and needs.
     </p>
   </div>
 
